5fd7a0ad838e140da20e3b8c854577b5300e5df1
Federico Parola <federico.parola@polito.it>
2023-08-03 00:57:48 +0000

    Add REST-based RC stub

--- test/app/rc_xapp.cpp
+++ test/app/rc_xapp.cpp
@@ -1,78 +1,109 @@
-// vi: ts=4 sw=4 noet:
-/*
-==================================================================================
-	Copyright (c) 2021 AT&T Intellectual Property.
-	Copyright (c) 2021 Alexandre Huff.
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <time.h>
+#include <unistd.h>
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+#define PORT 5000
+#define MAX_MSG_SIZE 4096
 
-       http://www.apache.org/licenses/LICENSE-2.0
+static char expected_req[] = "POST /api/echo HTTP/1.1\r\n";
+static char resp_template[] = "HTTP/1.1 200 OK\r\n"
+                              "Server: Custom/1.0.0 Custom/1.0.0\r\n"
+                              "Date: %s\r\n" /* current datetime */
+                              "Content-Length: %lu\r\n" /* body length */
+                              /* TODO:  what to put here? */
+                              "Connection: close\r\n"
+                               "\r\n"
+                              "%s"; /* body */
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-==================================================================================
-*/
+int main()
+{
+    int rc;
+    int lsock, sock;
+    ssize_t len;
+    char msg[MAX_MSG_SIZE], resp[MAX_MSG_SIZE];
 
-/*
-	Mnemonic:	rc_xapp.cpp
-	Abstract:   Implements a simple echo server just for testing gRPC calls
-                from TS xApp.
-
-	Date:		08 Dec 2021
-	Author:		Alexandre Huff
-*/
-
-#include <iostream>
-
-#include <grpc/grpc.h>
-#include <grpcpp/security/server_credentials.h>
-#include <grpcpp/server.h>
-#include <grpcpp/server_builder.h>
-
-#include "../../ext/protobuf/rc.grpc.pb.h"
-
-using namespace std;
-
-class ControlServiceImpl : public rc::MsgComm::Service {
-    ::grpc::Status SendRICControlReqServiceGrpc(::grpc::ServerContext* context, const ::rc::RicControlGrpcReq* request,
-                                                ::rc::RicControlGrpcRsp* response) override {
-
-        cout << "[RC] gRPC message received\n==============================\n"
-                << request->DebugString() << "==============================\n";
-
-        /*
-            TODO check if this is related to RICControlAckEnum
-            if yes, then ACK value should be 2 (RIC_CONTROL_ACK)
-            api.proto assumes that 0 is an ACK
-        */
-        response->set_rspcode(0);
-        response->set_description("ACK");
-
-        return ::grpc::Status::OK;
+    lsock = socket(AF_INET, SOCK_STREAM, 0);
+    if (lsock < 0) {
+        fprintf(stderr, "Error creating socket: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
     }
-};
 
-void RunServer() {
-    string server_address("0.0.0.0:50051");
-    ControlServiceImpl service;
+    int v = 1;
+    if (setsockopt(lsock, SOL_SOCKET, SO_REUSEPORT, &v, sizeof(v))) {
+        fprintf(stderr, "Unable to set SO_REUSEPORT sockopt\n");
+        exit(EXIT_FAILURE);
+    }
 
-    grpc::ServerBuilder builder;
-    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    builder.RegisterService(&service);
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(PORT);
+    if (bind(lsock, (struct sockaddr *)&addr, sizeof(addr))) {
+        fprintf(stderr, "Error binding to port %d: %s\n", PORT,
+                strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 
-    unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    if (listen(lsock, 10)) {
+        fprintf(stderr, "Error listening: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
 
-    cout << "[RC] Server listening on " << server_address << std::endl;
-    server->Wait();
-}
+    printf("[RC] Server listening on %d\n", PORT);
 
-int main(int argc, char const *argv[]) {
-    RunServer();
+    for (;;) {
+        sock = accept(lsock, NULL, NULL);
+        if (sock < 0) {
+            fprintf(stderr, "Error accepting connection: %s\n",
+                    strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
+        len = recv(sock, msg, MAX_MSG_SIZE, 0);
+        if (len <= 0) {
+            fprintf(stderr, "Error receiving message: %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        msg[len] = 0;
+
+        /* Check the header is good */
+        if (strncmp(msg, expected_req, sizeof(expected_req) - 1)) {
+            fprintf(stderr, "Received unexpected message:\n%s\n", msg);
+            exit(EXIT_FAILURE);
+        }
+
+        /* Locate body */
+        char *body = strstr(msg, "\r\n\r\n");
+        if (!body) {
+            fprintf(stderr, "Received request without body:\n%s", msg);
+            exit(EXIT_FAILURE);
+        }
+        body += 4; /* Skip newlines */
+
+        time_t t = time(NULL);
+        struct tm *tm = localtime(&t);
+        char timestr[64];
+        if (!strftime(timestr, sizeof(timestr), "%c", tm)) {
+            fprintf(stderr, "Error getting time string: %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
+        sprintf(resp, resp_template, timestr, strlen(body), body);
+        if (send(sock, resp, strlen(resp), 0) != strlen(resp)) {
+            fprintf(stderr, "Error sending message: %s\n", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
+        close(sock);
+    }
+
+    close(lsock);
 
     return 0;
 }
